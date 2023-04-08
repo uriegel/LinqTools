00fdc3c502e9ec41c74a85679e2e016d54405863
Uwe Riegel <uriegel@hotmail.de>
2023-04-08 15:59:02 +0200

    Separation between nullable Monad and nullable  extension methods

--- LinqTools/GenericNullable.cs
+++ LinqTools/GenericNullable.cs
@@ -3,18 +3,6 @@ namespace LinqTools.Nullable;
 public static class GenericNullable
 {
     /// <summary>
-    /// If not sure if a value is null although it is a not nullable, you can create a nullable Monad
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="t"></param>
-    /// <returns></returns>
-    public static T? AsNullable<T>(this T t)
-        where T : class
-        => t != null
-            ? t
-            : null;
-
-    /// <summary>
     /// Use nullable in LINQ query syntax
     /// </summary>
     /// <typeparam name="T"></typeparam>
@@ -132,32 +120,6 @@ public static class GenericNullable
             : default(RR?);
 
     /// <summary>
-    /// Get the value of a nullable if it is not null otherwise get a default value
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="t"></param>
-    /// <param name="defaultValue"></param>
-    /// <returns></returns>
-    public static T GetOrDefault<T>(this T? t, T defaultValue)
-        where T : class
-        => t != null
-            ? t
-            : defaultValue;
-
-    /// <summary>
-    /// Get the value of a nullable if it is not null otherwise get a default value
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="t"></param>
-    /// <param name="defaultValue"></param>
-    /// <returns></returns>
-    public static T GetOrDefault<T>(this T? t, T defaultValue)
-        where T : struct
-        => t.HasValue
-            ? t.Value
-            : defaultValue;
-
-    /// <summary>
     /// Filter an nullable. If filter condition is false, return null
     /// </summary>
     /// <typeparam name="T"></typeparam>
@@ -184,19 +146,6 @@ public static class GenericNullable
             : null;
 
     /// <summary>
-    /// If a value is null, call function 'elseWith' and take that value, otherwise take the first value
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="t"></param>
-    /// <param name="elseWith"></param>
-    /// <returns></returns>
-    public static T? OrElseWith<T>(this T? t, Func<T?> elseWith)
-        where T : class
-        => t != null
-            ? t
-            : elseWith();
-
-    /// <summary>
     /// Iterate through an nullable, that is call action only if value is != null
     /// </summary>
     /// <typeparam name="T"></typeparam>
@@ -220,28 +169,6 @@ public static class GenericNullable
     {
         if (opt.HasValue)
             action(opt.Value);
-    }
-}
-
-public static partial class Core
-{
-    /// <summary>
-    /// Create a nullable by calling a function. If an exception occurs the return value is null
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="func"></param>
-    /// <returns></returns>
-    public static T? ExceptionToNull<T>(Func<T> func)
-        where T : notnull
-    {
-        try
-        {
-            return func();
-        }
-        catch
-        {
-            return default(T?);
-        }
     }
 }
 
---
+++ LinqTools/NullableExtensions.cs
@@ -0,0 +1,76 @@
+public static class NullableExtensions
+{
+    /// <summary>
+    /// If not sure if a value is null although it is a not nullable, you can create a nullable Monad
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="t"></param>
+    /// <returns></returns>
+    public static T? AsNullable<T>(this T t)
+        where T : class
+        => t != null
+            ? t
+            : null;
+
+    /// <summary>
+    /// Get the value of a nullable if it is not null otherwise get a default value
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="t"></param>
+    /// <param name="defaultValue"></param>
+    /// <returns></returns>
+    public static T GetOrDefault<T>(this T? t, T defaultValue)
+        where T : class
+        => t != null
+            ? t
+            : defaultValue;
+
+    /// <summary>
+    /// Get the value of a nullable if it is not null otherwise get a default value
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="t"></param>
+    /// <param name="defaultValue"></param>
+    /// <returns></returns>
+    public static T GetOrDefault<T>(this T? t, T defaultValue)
+        where T : struct
+        => t.HasValue
+            ? t.Value
+            : defaultValue;
+
+    /// <summary>
+    /// If a value is null, call function 'elseWith' and take that value, otherwise take the first value
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="t"></param>
+    /// <param name="elseWith"></param>
+    /// <returns></returns>
+    public static T? OrElseWith<T>(this T? t, Func<T?> elseWith)
+        where T : class
+        => t != null
+            ? t
+            : elseWith();
+}
+
+public static partial class Core
+{
+    /// <summary>
+    /// Create a nullable by calling a function. If an exception occurs the return value is null
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="func"></param>
+    /// <returns></returns>
+    public static T? ExceptionToNull<T>(Func<T> func)
+        where T : notnull
+    {
+        try
+        {
+            return func();
+        }
+        catch
+        {
+            return default(T?);
+        }
+    }
+}
+
