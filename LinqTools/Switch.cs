506800d4ff5bfb9c8961fd619e97167bc35e2ce9
Uwe Riegel <uriegel@hotmail.de>
2023-08-12 13:13:59 +0200

    Result at the end

--- LinqTools/Switch.cs
+++ LinqTools/Switch.cs
@@ -2,17 +2,17 @@ namespace LinqTools;
 
 static class ChooseExtensions
 {
-    public static Option<TResult> Choose<TResult, T>(this T t, params SwitchType<TResult, T>[] switches)
+    public static Option<TResult> Choose<TResult, T>(this T t, params SwitchType<T, TResult>[] switches)
         where TResult : notnull
         => switches
             .FirstOrNone(s => s.Predicate(t))
             .Select(s => s.Selector(t));
 
-    public record SwitchType<TResult, T>(Predicate<T> Predicate, Func<T, TResult> Selector)
+    public record SwitchType<T, TResult>(Predicate<T> Predicate, Func<T, TResult> Selector)
     {
-        public static SwitchType<TResult, T> Switch(Predicate<T> predicate, Func<T, TResult> selector)
+        public static SwitchType<T, TResult> Switch(Predicate<T> predicate, Func<T, TResult> selector)
             => new(predicate, selector);
-        public static SwitchType<TResult, T> Default(Func<T, TResult> selector)
+        public static SwitchType<T, TResult> Default(Func<T, TResult> selector)
             => new(_ => true, selector);
     };
 }
